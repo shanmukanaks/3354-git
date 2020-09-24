747354a39b41260c4ce0d4eb53898f60ea2f7b01
shanmukanaks <shanmukanaks@gmail.com>
2020-09-24 16:16:58 -0500

    Created experimental changes branch and rewrote Main.java
    Created the experimental changes branch and modified the Main.java file to subtract
    positive integers if the first argument is a dash.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,30 +1,40 @@
 package adder;
+
 public class Main {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         try {
             int result = addArguments(args);
-            if (args.length==0)
-                throw new ArrayIndexOutOfBoundsException();
             System.out.println(result);
-    	}
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.err.println("Not enough arguments");
-    	}
-        catch(NumberFormatException n) {
-                System.out.println("Invalid characters");
-    	}
-    }
-    private static int addArguments(String[] args) {
-        
-        int sum=0;
-        
-        for(int i = 0;i < args.length;i++)
-        {
-            sum = sum + Integer.valueOf(args[i]);
-    	}
-    	
-        return sum;
+	} catch (IllegalArgumentException e) {
+	    System.err.println("Please enter integers.");
+        } catch (Exception e) {
+            System.err.println("Please enter an argument(s).");
+        }
     }
 
+   private static int addArguments(String[] args) {
+       
+       int sum=0;
+       int sign=0;
+       int index=0;
+       
+       if(args[0].equals("-"))
+       {
+           sign = 1;
+           index = 1;
+       }
+       
+       for(int i = index;i < args.length;i++){
+           if(sign == 0)
+               sum += Integer.valueOf(args[i]) ;
+           if(sign == 1)
+               sum -= Integer.valueOf(args[i]) ;
+
+       }
+       
+       return sum;
+   }
 }
+
+
