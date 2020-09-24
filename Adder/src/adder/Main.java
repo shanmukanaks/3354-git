8729e5cba8d24fa4cb320c2eed7f51436e241955
shanmukanaks <shanmukanaks@gmail.com>
2020-09-24 15:51:37 -0500

    Implement new function into Main.java
    Modified Main.java to add an arbitrary number of positive integers.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -2,16 +2,37 @@ package adder;
 
 public class Main {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         try {
             int result = addArguments(args);
             System.out.println(result);
+	} catch (IllegalArgumentException e) {
+	    System.err.println("Please enter integers.");
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please enter an argument(s).");
         }
     }
 
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
-    }
+   private static int addArguments(String[] args) {
+       
+       int sum=0;
+       int sign=0;
+       int index=0;
+       
+       if(args[0].equals("-"))
+       {
+           sign = 1;
+           index = 1;
+       }
+       
+       for(int i = index;i < args.length;i++){
+           if(sign == 0)
+               sum += Integer.valueOf(args[i]) ;
+           if(sign == 1)
+               sum -= Integer.valueOf(args[i]) ;
+
+       }
+       
+       return sum;
+   }
 }
